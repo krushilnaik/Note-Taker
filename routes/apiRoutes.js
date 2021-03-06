7ee8d8338db1d5c430e59c55d98d763a031eb8ec
Krushil <krushilnaik96@gmail.com>
2021-03-05 19:13:36 -0500

    implement post and delete requests to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 // import items needed
 const router = require('express').Router();
+const fs = require("fs");
+
 const store = require('../db/store')
 
 // make a GET request with all notes from the database
@@ -15,7 +17,42 @@ router.get('/notes', (req, res) => {
 
 // create a post request
 
+router.post("/notes", (req, res) => {
+	console.log("attempting post request");
+	store
+		.getNotes()
+		.then(notes => {
+		/**
+		 * @type {object[]}
+		 */
+		let noteList = notes;
+
+		let note = req.body;
+		note.id = noteList.length;
+
+		noteList.push(note);
+		console.log(`noteList is now:`, noteList);
+		console.log(JSON.stringify(noteList, null, "\t"));
+
+		fs.writeFile("./db/db.json", JSON.stringify(noteList, null, "\t"), err => res.json(err));
+	}).catch((err) => res.status(500).json(err));
+});
+
 // create a delete request
 
+router.delete("/notes/:id", (req, res) => {
+	store.getNotes().then(notes => {
+		/**
+		 * @type {object[]}
+		 */
+		let noteList = notes;
+
+		const removed = noteList.splice(Number(req.params.id), 1);
+
+		fs.writeFile("./db/db.json", JSON.stringify(noteList, null, "\t") + "\n", err => res.json(err));
+
+		console.log(`Removed ${removed} from database.`);
+	});
+});
 
 module.exports = router;
